8b588b45b21a028c5edc6e8a82a3dc02be9213d1
samuelgil007 <sagilar15@gmail.com>
2021-05-14 18:39:07 -0500

    Vista de recuperar contraseña y conección a firebase

--- app/src/main/java/co/edu/udea/compumovil/gr04_20211/paqueteriareg/RecuperarActivity.java
+++ app/src/main/java/co/edu/udea/compumovil/gr04_20211/paqueteriareg/RecuperarActivity.java
@@ -3,33 +3,26 @@ package co.edu.udea.compumovil.gr04_20211.paqueteriareg;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-import com.skydoves.elasticviews.ElasticCheckButton;
+public class RecuperarActivity extends AppCompatActivity {
 
-public class AdminActivity extends AppCompatActivity {
-
-    ElasticCheckButton btnEnviar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);
+        setContentView(R.layout.activity_recuperar);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.txt_titulo);
         //Activar boton back
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+
     }
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
-    }
-    public void getRecuperar(View view) {
-                Intent i = new Intent(AdminActivity.this, RecuperarActivity.class);
-                startActivity(i);
     }
 }
